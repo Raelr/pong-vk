11d07278276f2c6ec5eee3be6133a4183fbfcafa
Aryeh <aryehzinn@hotmail.com>
2020-06-17 17:13:04 +1000

    Got GLM to link properly.

--- premake5.lua
+++ premake5.lua
@@ -1,20 +1,24 @@
+
 workspace "Pong"
-    configurations { "Debug", "Release" }
+configurations { "Debug", "Release" }
 
 project "Pong"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    targetdir "bin/%{cfg.buildcfg}"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++17"
+targetdir "bin/%{cfg.buildcfg}"
 
-    files { "src/**.h", "src/**.cpp" }
-    links { "$(VULKAN_SDK)/lib/vulkan-1.lib" }
-    includedirs { "$(VULKAN_SDK)/include" }
+files { "src/**.h", "src/**.cpp" }
+links { 
+    "$(VULKAN_SDK)/lib"
+}
 
-    filter "configurations:Debug"
-         defines { "DEBUG" }
-         symbols "On"
+includedirs { "$(VULKAN_SDK)/include", "vendor/glm" }
 
-    filter "configurations:Release"
-        defines { "NDEBUG" }
-        optimize "On"
+filter "configurations:Debug"
+     defines { "DEBUG" }
+     symbols "On"
+
+filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <vulkan/vulkan.h>
+#include <glm/vec4.hpp> // glm::vec4
+#include <iostream>
+
 int main() {
+
+    glm::vec4 vec;
+    vec.x = 10;
+    vec.y = 11;
+    vec.z = 12;
+    vec.a = 13;
+
+    std::cout << "x: " << vec.x << " y: " << vec.y << " z: " << vec.z << std::endl;
     return 0;
 }
