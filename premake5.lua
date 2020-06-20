a32227f8a341119f2de038ede686ab951d219118
Aryeh <aryehzinn@hotmail.com>
2020-06-20 20:55:27 +1000

    Rest main.cpp to start setting up triangle rendering

--- premake5.lua
+++ premake5.lua
@@ -1,50 +1,49 @@
 
 workspace "Pong"
-configurations { "Debug", "Release" }
+     configurations { 
+          "Debug", 
+          "Release" 
+     }
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "vendor/GLFW/"
 
 project "Pong"
-kind "ConsoleApp"
-language "C++"
-cppdialect "C++17"
-targetdir "bin/%{cfg.buildcfg}"
+     kind "ConsoleApp"
+     language "C++"
+     cppdialect "C++17"
+     targetdir "bin/%{cfg.buildcfg}"
 
-files { "src/**.h", "src/**.cpp" }
+     files { "src/**.h", "src/**.cpp" }
 
-links {
-     "GLFW",
-}
-
-includedirs { 
-     "$(VULKAN_SDK)/include",
-     "vendor/glm",
-     "vendor/GLFW/glfw/include",
-}
-
-filter "system:macosx"
-    systemversion "latest"
-    staticruntime "on"
-    
-    linkoptions { os.getenv("VULKAN_SDK") .. "lib/libvulkan.1.2.135.dylib" }
-
-    defines { 
-         "VK_ICD_FILENAMES=" .. os.getenv("VULKAN_SDK") .. "share/vulkan/icd.d/MoltenVK_icd.json",
-         "VK_LAYER_PATH=" .. os.getenv("VULKAN_SDK") .. "share/vulkan/explicit_layer.d"
+     links {
+          "GLFW",
      }
 
-    links {
-      "Cocoa.framework",
-      "IOKit.framework",
-      "CoreFoundation.framework",
-    }
+     includedirs { 
+          "$(VULKAN_SDK)/include",
+          "vendor/glm",
+          "vendor/GLFW/glfw/include",
+     }
 
-filter "configurations:Debug"
-     defines { "DEBUG" }
-     symbols "On"
+     filter "system:macosx"
+          systemversion "latest"
+          staticruntime "on"
+          linkoptions { os.getenv("VULKAN_SDK") .. "lib/libvulkan.1.2.135.dylib" }
+          defines { 
+               "VK_ICD_FILENAMES=" .. os.getenv("VULKAN_SDK") .. "share/vulkan/icd.d/MoltenVK_icd.json",
+               "VK_LAYER_PATH=" .. os.getenv("VULKAN_SDK") .. "share/vulkan/explicit_layer.d"
+          }
+          links {
+               "Cocoa.framework",
+               "IOKit.framework",
+               "CoreFoundation.framework",
+          }
 
-filter "configurations:Release"
-    defines { "RELEASE" }
-    optimize "On"
+     filter "configurations:Debug"
+          defines { "DEBUG" }
+          symbols "On"
 
+     filter "configurations:Release"
+          defines { "RELEASE" }
+          optimize "On"
