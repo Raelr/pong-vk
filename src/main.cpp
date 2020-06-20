6309e06c17725b8c6da8276e559e09b383b23eed
Aryeh <aryehzinn@hotmail.com>
2020-06-21 09:58:03 +1000

    Initiualised Vulkan instance with GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
 #include <iostream>
 #include <cstdlib>
 
@@ -9,6 +8,9 @@ const int WINDOW_HEIGHT = 600;
 const int WINDOW_WIDTH = 800;
 
 int main() {  
+
+    // -------------------- INITIALISE WINDOW --------------------------
+
     // Initialise GLFW
     glfwInit();
 
@@ -20,10 +22,51 @@ int main() {
     // Actually make the window
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong", nullptr, nullptr);
 
+    // ------------------ Initialise Vulkan instance ---------------------
+
+    // Initialise the Vulkan instance struct. 
+    VkInstance instance; 
+
+    // Define the configuration details of the vulkan application. 
+    VkApplicationInfo appInfo {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Pong";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_2;
+
+    // Configuration parameters for Vulkan instance creation.
+    VkInstanceCreateInfo createInfo {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Now we need to get the extensions required by GLFW in order for it to work with Vulkan.
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // Set the extensions in the configuration struct. 
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // Set validation layer count to 0 for now.
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        printf("FAILED TO CREATE VULKAN INSTANCE!");
+        return EXIT_FAILURE;
+    }
+
+    // ------------------------- MAIN LOOP ------------------------------
+
     // Start the loop and only stop when a close event happens.
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
+
+    // --------------------------- CLEANUP ------------------------------
 
     // Cleaning up memory
     glfwDestroyWindow(window);
