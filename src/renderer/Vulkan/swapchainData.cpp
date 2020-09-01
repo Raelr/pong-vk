aa03b532216160584ef9d8ded50b25c5719afe95
Aryeh <aryehzinn@hotmail.com>
2020-09-02 08:32:35 +1000

    Started moving swapchain data out of vulkanUtils file. Project is currently broken

---
+++ src/renderer/Vulkan/swapchainData.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aryeh Zinn on 2/9/20.
+//
+
+#include "swapchainData.h"
---
+++ src/renderer/Vulkan/swapchainData.h
@@ -0,0 +1,16 @@
+//
+// Created by Aryeh Zinn on 2/9/20.
+//
+
+#ifndef PONG_VK_SWAPCHAINDATA_H
+#define PONG_VK_SWAPCHAINDATA_H
+
+
+
+class swapchainData {
+
+};
+
+
+
+#endif //PONG_VK_SWAPCHAINDATA_H
